530f3e84173c7e714f5e457c6def8a8b1415bfd6
Timothy <timothy.wong@hotmail.co.uk>
2018-08-28 13:47:51 +0100

    updated variable name

--- app.R
+++ app.R
@@ -15,6 +15,7 @@ library(ggplot2)
 library(forcats)
 library(nycflights13)
 library(rpart)
+library(plotly)
 
 myUniqueOrigin <- flights$origin %>% unique() %>% sort()
 myUniqueDest <- flights$dest %>% unique() %>% sort()
@@ -29,68 +30,67 @@ myFlightModel <- rpart(dep_delay ~
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Flight Delay Calculator"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        dateInput("myDate",
-                  "Departure Date",
-                  value = make_date(2019, 1, 1)),
-        
-        radioButtons("myOrigin",
-                     "Depart from",
-                     choices = myUniqueOrigin, 
-                     selected = myUniqueOrigin[1]),
-        
-        selectInput("myDest", 
-                    "Arrive at", 
-                    choices = myUniqueDest, 
-                    selected = myUniqueDest[1]),
-        
-        selectInput("myCarrier", 
-                    "Carrier", 
-                    choices = myUniqueCarrier, 
-                    selected = myUniqueCarrier[1])
-      ),
+  
+  # Application title
+  titlePanel("Flight Delay Calculator"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      dateInput("myDate",
+                "Departure Date",
+                value = make_date(2019, 1, 1)),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+      radioButtons("myOrigin",
+                   "Depart from",
+                   choices = myUniqueOrigin, 
+                   selected = myUniqueOrigin[1]),
+      
+      selectInput("myDest", 
+                  "Arrive at",
+                  choices = myUniqueDest, 
+                  selected = myUniqueDest[1]),
+      
+      selectInput("myCarrier",
+                  "Carrier",
+                  choices = myUniqueCarrier, 
+                  selected = myUniqueCarrier[1])
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotlyOutput("myPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  myNewFlighReactive <- reactive({
+  myNewFlightReactive <- reactive({
     tibble(date = input$myDate,
            hour= 0:23,
            origin=input$myOrigin,
            dest = input$myDest, 
            carrier= input$myCarrier)})
   
-   output$distPlot <- renderPlot({
-     
-     myNewFlightDelay <- predict(myFlightModel, myNewFlighReactive())
-     
-     tibble(hour=0:23, dep_delay = myNewFlightDelay) %>%
-       ggplot(aes(x=hour, y=dep_delay)) +
-       geom_col() +
-       scale_y_continuous(limits = c(-10,60)) + 
-       labs(x="Planned Departure Hour",
-            y="Expected Departure Delay (Minutes)",
-            title=sprintf("%s flight from %s to %s in %s (%s)",
-                          input$myCarrier,
-                          input$myOrigin,
-                          input$myDest,
-                          input$myDate %>% months(TRUE),
-                          input$myDate %>% weekdays(TRUE)))
-   })
+  output$myPlot <- renderPlotly({
+    
+    myNewFlightDelay <- predict(myFlightModel, myNewFlightReactive())
+    
+    tibble(hour=0:23, dep_delay = myNewFlightDelay) %>%
+      ggplot(aes(x=hour, y=dep_delay)) +
+      geom_col() +
+      scale_y_continuous(limits = c(-10,60)) + 
+      labs(x="Planned Departure Hour",
+           y="Expected Departure Delay (Minutes)",
+           title=sprintf("%s flight from %s to %s in %s (%s)",
+                         input$myCarrier,
+                         input$myOrigin,
+                         input$myDest,
+                         input$myDate %>% months(TRUE),
+                         input$myDate %>% weekdays(TRUE)))
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
